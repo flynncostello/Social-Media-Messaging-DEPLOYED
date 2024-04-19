6e3c2dae61a1a39d1149aed9116eba4bdd88e819
Flynn Costello <flynn.p.costello@outlook.com>
2024-04-19 14:50:06 +1000

    updated API endpoint

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,4 +2,4 @@ import axios from 'axios';
 axios.defaults.withCredentials = true;
 export default axios;
 
-export const API_ENDPOINT = "https://localhost:3000/api";
+export const API_ENDPOINT = "https://social-media-messaging-deployed.onrender.com/api";
